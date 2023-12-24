0ded638bfd2e67771680b4dbf0886c7460c3e166
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2023-12-24 18:37:25 +1300

    Fixed maths

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 
 // The code equivalent of a one page TTRPG
 
+Console.Title = "CPU Plays Pokemon";
+
 Console.WriteLine("""
     CPU Plays Pokemon! 
     Technically, CPU Plays anything to do with mGBA.
@@ -12,14 +14,16 @@ Console.WriteLine("""
 
     """);
 
-var percentagePart = PercentagePart.Fraction;
-var platform = 1;
+// 0 for GBA, 1 for GB
+var platform = 0;
 var platformModulo = platform switch
 {
     0 => 10,
     1 => 8,
     _ => throw new Exception("No platform")
 };
+
+var httpClient = new HttpClient();
 
 // Post Framework, this needs to be added via nuget package
 var cpuPerformanceCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
@@ -31,15 +35,17 @@ var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
 while (await timer.WaitForNextTickAsync())
 {
     var cpuPercent = cpuPerformanceCounter.NextValue();
-    var valueToUse = GetUsableCpuValue(percentagePart, cpuPercent);
+    var valueToUse = (int)Math.Truncate(cpuPercent) + int.Parse(cpuPercent.ToString().Split(".")[^1]);
     var keyInt = valueToUse % platformModulo;
 
-    Console.WriteLine($"CPU%: {cpuPercent,9} (using {valueToUse,6}). Modulo {platformModulo} = {keyInt}: {(KeysEnum)keyInt}");
+    Console.WriteLine($"CPU%: {cpuPercent,10} (using {valueToUse,7}). Modulo {platformModulo} = {keyInt}: {(KeysEnum)keyInt}");
+
+    await httpClient.PostAsync($"http://localhost:5000/mgba-http/button?key={(KeysEnum)keyInt}", null);
+
+    //await httpClient.PostAsJsonAsync($"http://localhost:5000/mgba-http/button", (KeysEnum)keyInt);
+
 }
 
-int GetUsableCpuValue(PercentagePart percentagePart, float cpuPercent) => percentagePart == PercentagePart.Whole 
-    ? (int)Math.Truncate(cpuPercent) 
-    : int.Parse(cpuPercent.ToString().Split(".")[^1]);
 
 enum KeysEnum
 {
@@ -53,10 +59,4 @@ enum KeysEnum
     Down,
     R,
     L
-}
-
-enum PercentagePart
-{
-    Whole,
-    Fraction
 }
